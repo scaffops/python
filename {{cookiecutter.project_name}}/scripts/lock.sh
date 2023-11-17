7dbe9c1a36cf648fbaab5d2ea52f27eaa2201ff0
bswck <bswck.dev@gmail.com>
2023-11-17 05:34:08 +0100

    Add lock script

---
+++ {{cookiecutter.project_name}}/scripts/lock.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# (C) 2023–present Bartosz Sławecki (bswck)
+# A manual alias for long poetry commands ✨
+#
+# Run this before pushing to ensure that package locks are up-to-date.
+# We update requirements-dev.txt in order to make sure that the CI environment
+# can install the dev dependencies without Poetry.
+#
+# Usage:
+# $ poe lock
+poetry lock
+poetry export --with=dev -f requirements.txt -o requirements-dev.txt
