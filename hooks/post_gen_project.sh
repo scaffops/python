6970fef8122ca5b0f00504999d2d7cee09c35a9d
bswck <bswck.dev@gmail.com>
2023-11-17 06:23:18 +0100

    Fix post_gen_project.sh hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -20,6 +20,6 @@ poetry run pre-commit install --hook-type pre-commit --hook-type pre-push
 git add .
 git commit -m "Initial commit"
 gh repo create {{ cookiecutter.repo_name }} --{{ cookiecutter.visibility }} --source=./ --remote=upstream
-git branch -M master
+git checkout -b master
 git remote add origin https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}.git
 git push -u origin master
