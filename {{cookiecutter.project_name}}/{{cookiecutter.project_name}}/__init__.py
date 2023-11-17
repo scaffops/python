a107ef99abe4eeafda000c5da99eab5c8fc6ef04
bswck <bswck.dev@gmail.com>
2023-11-17 06:15:03 +0100

    Change `__init__.py` docstring template

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
@@ -1,5 +1,6 @@
 """
-{{cookiecutter.project_name}}.{% if cookiecutter.project_description %}{{"\n\n"}}
+{{cookiecutter.project_name}}.{% if cookiecutter.project_description %}{"\n"}
 {{cookiecutter.project_description}}{% endif %}
+
 (C) 2023-present {{cookiecutter.full_name}} ({{cookiecutter.github_username}})
 """
