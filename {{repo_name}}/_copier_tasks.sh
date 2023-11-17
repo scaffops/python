970d6fe0e8befb7f1b4b4161ab8e6ba04339dd6f
bswck <bswck.dev@gmail.com>
2023-11-17 20:01:49 +0100

    Finalize tasks

--- {{repo_name}}/_copier_tasks.sh
+++ {{repo_name}}/_copier_tasks.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # (C) 2023–present Bartosz Sławecki (bswck)
 #
-# Setup a new project with Poetry and GitHub.
 # This script is run automatically by copier.
 # As of 17.11.2023, https://github.com/copier-org/copier/issues/240 is not resolved.
 echo "Checking if this directory is a git repository..."
 git rev-parse --is-inside-work-tree
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then  # $OPERATION=copy
     git init .
     poetry install
     if [ $? -eq 0 ]; then
@@ -18,13 +17,14 @@ if [ $? -ne 0 ]; then
     PYTHON_VERSION="$(cat .python-version)"
     poetry env use $PYTHON_VERSION
     poetry run poe lock
+    poetry run poe hook copy
     git add .
     git commit -m "Initial commit"
     gh repo create {{ repo_name }} --{{ visibility }} --source=./ --remote=upstream
     git remote add origin https://github.com/{{ github_username }}/{{ repo_name }}.git
     git push -u origin master
     poetry run pre-commit install --hook-type pre-commit --hook-type pre-push
-else
+else  # $OPERATION=update
     echo "This directory is a git repository."
-    echo "Initial commit and remote setup skipped."
+    poetry run poe hook update
 fi
