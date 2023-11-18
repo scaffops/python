9f5df0c1f80c2b61e91cdf6b80dbf3cb5dd1e72a
bswck <bswck.dev@gmail.com>
2023-11-18 15:35:05 +0100

    Fix copier hook and add git add command

--- _copier_hook_task.sh
+++ _copier_hook_task.sh
@@ -2,7 +2,7 @@ echo Running copier hook...
 poetry run python copier_hook.py
 echo Copier hook exited with code $?.
 echo Removing copier hook...
-poetry run rm copier_hook.py
+rm copier_hook.py
 ls
 if [ $? -eq 0 ]; then
     echo Copier hook removed.
