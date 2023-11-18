90ef9db26b727d6f09f86511dd11ba139781e75f
bswck <bswck.dev@gmail.com>
2023-11-18 14:19:04 +0100

    Update copier hook and add pre-commit and pre-push hooks

--- _copier_hook_task.sh
+++ _copier_hook_task.sh
@@ -1,11 +1,11 @@
-echo "Running copier hook..."
+echo Running copier hook...
 poetry run python copier_hook.py
-echo "Copier hook exited with code "$?"."
-echo "Removing copier hook..."
+echo Copier hook exited with code $?.
+echo Removing copier hook...
 rm copier_hook.py
 if [ $? -eq 0 ]; then
-    echo "Copier hook removed."
+    echo Copier hook removed.
 else
-    echo "Failed to remove copier hook."
+    echo Failed to remove copier hook.
     exit 1
 fi
