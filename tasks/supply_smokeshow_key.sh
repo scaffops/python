c5f67e89018c54a88f699ea9450112ce79b86541
bswck <bswck.dev@gmail.com>
2023-11-18 21:36:29 +0100

    Refactor smokeshow secret creation process

--- tasks/supply_smokeshow_key.sh
+++ tasks/supply_smokeshow_key.sh
@@ -1,9 +1,9 @@
-echo "Check if smokeshow secret needs to be created.."
+echo "Checking if smokeshow secret needs to be created..."
 if test "$(gh secret list -e Smokeshow | grep -o SMOKESHOW_AUTH_KEY)"; then
-  echo "Smokeshow secret already exists"
+  echo "Smokeshow secret already exists, aborting."
   exit 0
 fi
 echo "Smokeshow secret does not exist, creating..."
 SMOKESHOW_AUTH_KEY=$(smokeshow generate-key | grep SMOKESHOW_AUTH_KEY | grep -oP "='\K[^']+")
 gh secret set SMOKESHOW_AUTH_KEY --env Smokeshow --body "$SMOKESHOW_AUTH_KEY"
-echo "Smokeshow secret created."
+[$? -eq 0] && echo "Smokeshow secret created." || echo "Failed to create smokeshow secret."
