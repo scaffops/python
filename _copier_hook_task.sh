0cafede9cb7fe547aeb8f35c9528ad749a8dcd97
bswck <bswck.dev@gmail.com>
2023-11-18 14:12:38 +0100

    Add copier hook task to remove copier hook

--- _copier_hook_task.sh
+++ _copier_hook_task.sh
@@ -1,2 +1,11 @@
+echo "Running copier hook..."
 poetry run python copier_hook.py
+echo "Copier hook exited with code "$?"."
+echo "Removing copier hook..."
 rm copier_hook.py
+if [ $? -eq 0 ]; then
+    echo "Copier hook removed."
+else
+    echo "Failed to remove copier hook."
+    exit 1
+fi
