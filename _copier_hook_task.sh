8b7629f57b30ab8dc5a1c7b8fb6aa2fdba318052
bswck <bswck.dev@gmail.com>
2023-11-18 15:33:01 +0100

    Add command to list files after removing copier hook

--- _copier_hook_task.sh
+++ _copier_hook_task.sh
@@ -3,6 +3,7 @@ poetry run python copier_hook.py
 echo Copier hook exited with code $?.
 echo Removing copier hook...
 poetry run rm copier_hook.py
+ls
 if [ $? -eq 0 ]; then
     echo Copier hook removed.
 else
