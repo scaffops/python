8a70648f094a5a7a642ac8bbbc22f9752886daf5
bswck <bswck.dev@gmail.com>
2023-11-18 23:24:47 +0100

    Don't use poetry env in copier_hook.py

--- tasks/copier_hook.sh
+++ tasks/copier_hook.sh
@@ -1,11 +1,8 @@
 echo "Running copier hook..."
-poetry run python copier_hook.py
+python copier_hook.py
 echo "Copier hook exited with code $?."
 echo "Removing copier hook..."
 rm copier_hook.py
-{% if poetry_cleanup %}
-poetry env remove python
-{% endif %}
 if [ $? -eq 0 ]; then
     echo Copier hook removed.
 else
