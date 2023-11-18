83df9d167dd20634bb27462d72d627e0098e6a62
bswck <bswck.dev@gmail.com>
2023-11-18 17:22:38 +0100

    Generate smokeshow key when needed

---
+++ tasks/supply_smokeshow_key.sh
@@ -0,0 +1,9 @@
+echo Check if smokeshow secret needs to be created..
+if test "$(gh secret list -e Smokeshow | grep -o SMOKESHOW_AUTH_KEY)"; then
+  echo "Smokeshow secret already exists"
+  exit 0
+fi
+echo "Smokeshow secret does not exist, creating..."
+SMOKESHOW_AUTH_KEY=$(smokeshow generate-key | grep SMOKESHOW_AUTH_KEY | grep -oP "='\K[^']+")
+gh secret set SMOKESHOW_AUTH_KEY --env Smokeshow --body "$SMOKESHOW_AUTH_KEY"
+echo "Smokeshow secret created."
