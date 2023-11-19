4450b4369c717ce19aa95a59a7d5d7e4c64b9e78
bswck <bswck.dev@gmail.com>
2023-11-19 05:05:22 +0100

    Fix N/A project path

--- handle-task-stage.sh
+++ handle-task-stage.sh
@@ -22,9 +22,10 @@ setup_task_stage() {
     fi
 
     determine_project_path
+    local PROJECT_PATH=
     echo
     echo "--- Task stage: $TASK_STAGE"
-    echo "--- Project path: "$("$PROJECT_PATH" || "N/A")
+    echo "--- Project path: ${PROJECT_PATH:-"N/A"}"
     echo
 }
 
